8100ab2a3539870cfdf8a2f270b96d8b5b67f4ff
satish-100 <58321278+satish-100@users.noreply.github.com>
2024-10-30 09:55:28 +0530

    Create responsive.js

---
+++ responsive.js
@@ -0,0 +1,44 @@
+Font- Sizes -
+
+//10
+text-xxs lg:text-xxxs xl:text-xxs 2xl:text-xs 3xl:text-sm
+
+//12
+text-xs lg:text-xxs xl:text-xs 2xl:text-sm 3xl:text-base
+
+//14
+text-sm lg:text-xs xl:text-sm 2xl:text-base 3xl:text-lg
+
+//15px
+text-[0.9375rem] lg:text-[0.8125rem] xl:text-text-[0.9375rem] 2xl:text-[1.0625rem] 3xl:text-[1.1875rem]
+
+//16px
+text-base lg:text-sm xl:text-base 2xl:text-lg 3xl:text-xl
+
+// 18px
+text-lg lg:text-base xl:text-lg 2xl:text-xl 3xl:text-2xl
+
+// 20px
+text-xl lg:text-lg xl:text-xl 2xl:text-2xl 3xl:text-3xl
+
+// 22px
+text-[1.375rem] leading-[1.5rem] lg:text-xl xl:text-[1.375rem] xl:leading-8 2xl:text-2xl 3xl:text-3xl
+
+// 24px
+text-2xl lg:text-xl xl:text-2xl 2xl:text-3xl 3xl:text-4xl
+
+// 26px
+text-[1.625rem] leading-9 lg:text-2xl xl:text-[1.625rem] xl:leading-9 2xl:text-3xl 3xl:text-4xl
+
+text-[2.5rem] lg:text-[2.375rem] xl:text-[2.5rem] 2xl:text-[2.625rem] 3xl:text-[2.75rem]
+
+text-[1.625rem] lg:text-2xl xl:text-[1.625rem] 2xl:text-[1.75rem] 3xl:text-[1.875rem] leading-none
+
+//32px
+text-[2rem] lg:text-[1.875rem] xl:text-[2rem] 2xl:text-[2.125rem] 3xl:[2.25rem]
+
+text-[3rem] lg:text-[2.875rem] xl:text-[3rem] 2xl:text-[3.125rem] 3xl:text-[3.25rem]
+
+
+
+text-[3.5rem] lg:text-[3.375rem] xl:text-[3.5rem] 2xl:text-[3.625rem] 3xl:text-[3.75rem]
